60db3fa82e6f78847c34e72bc54c171e3b36a46b
vmuser <vmuser@trustyvm.cs.virginia.edu>
2018-04-24 04:50:09 -0400

    next level

--- up.cpp
+++ up.cpp
@@ -15,6 +15,8 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <regex>
+
 
 #define null 0;
 using namespace std;
@@ -36,53 +38,7 @@ using namespace std;
   pid_t pid;
   pid_t pipeid;
   pid_t pids[50] = {0};
-  //int pid_status = 0;
-// char * inputParsing() {
-//   fflush(stdout)
-//   int index = 0;
-//   char * inputbuffer = malloc(sizeof(char) * 100);
-//   
-//   char current = "";
-//   while(True){
-//     current = getchar();
-//     
-//     if (current == EOF || c == '\n' ){
-//       inputbuffer[index] = '\0';
-//       inputbuffer = trimwhitespace(inputbuffer)
-//       return inputbuffer;
-//     }
-//     else {
-//       inputbuffer[index] = current;
-//       index += 1;
-//     }
-//     if (index == 100){
-//       cerr << "line size exceeds 100" << endl;
-//       //check this out 
-//       exit(EXIT_FAILURE);
-//     }
-//     
-//   
-// }
-// 
-// }
 
-char *trimwhitespace(char *str){
-  char *end;
-// Trim leading space
-  while(isspace(*str)) str++;
-
-  if(*str == 0){ // All spaces?
-    return str;
-
-  }// Trim trailing space
-  end = str + strlen(str) - 1;
-  while(end > str && isspace(*end)){
-    end--;
-  }
-// Write new null terminator
-  *(end+1) = '\0';
-  return str;
-}
 
 int stringToTokens(string command, string* argumentP) {
   cout << " the command is " << command << endl;
@@ -97,56 +53,49 @@ int stringToTokens(string command, string* argumentP) {
   
       return -1;
     }
-// iterate over string
+
     while (i < endPosition) {
-     // cout << i << " tokenizer" << endl;
-// increment i until a non-WS char is found
+
       while ((command[i] == ' ' || command[i] == '\t')) {
 	i += 1;
-//	cout << "still in this space loop" << endl;
+
 	}
-// set beginning of token to current pos -- first non-WS char
+
       int tokenBeginIndex = i;
-// increment i until a WS char is found
+
       while (command[i] != ' ' && command[i] != '\t' && i <= endPosition){
 	i += 1;
-//	cout << i << endl;
 	}
-// set token end index to current pos -- first WS char
+
       int tokenEndIndex = i;
-      string token = command.substr((unsigned long) tokenBeginIndex, (unsigned long) (tokenEndIndex - tokenBeginIndex));
+      string token = command.substr(tokenBeginIndex,  (tokenEndIndex - tokenBeginIndex));
       cout << token << " the current token" << endl;
-      //if (isValidToken(token))
-// save the token into the token array
-  //    cout << "the token is " << token << endl;
-   //   cout << "index value is " << current << endl;
       argumentP[current] = token;
-    //  cout << "allocation was fine" << endl;
-// increment the pointer index
+
       if (token == ">" || token == "<"){
 	modified = current;
       }
       current += 1;
     }
     current -= 1;
-   // cout << "at the bottom" << endl;
-cout << "number of tokens is " << current << endl;
+
+//cout << "number of tokens is " << current << endl;
 if (modified != 0){
-  cout << "ACTUAL NUMBER WITH REDIRECT " << modified << endl;
-  //return (modified -1);
+  //cout << "ACTUAL NUMBER WITH REDIRECT " << modified << endl;
+
 }
   return current;
 
 }
 
 char ** parseLine(char * raw){
-  //cout << "inside belly of the beast" << endl;
+
   valid = true;
   int index = 0;
   int argVal = 0;
   int size = 100;
   char **tokens = (char **)malloc(size *sizeof(char*));
-  //char * currtoken;
+
   int hasOut = 0;
   int hasIn = 0; 
   int starting = 0;
@@ -159,26 +108,14 @@ char ** parseLine(char * raw){
     valid = false;
     cout << "INVALID STRING" << endl;
     return null;
-    //cannot end with a operations or begin with an operation
-    
+
   }
   
-  //  cout << "before the for loop!" << endl;
-   // cout << strlen(raw) << endl;
+
     string curr = "";
     for (int i = 0; i < strlen(raw); i++){
       
-     // cout << "i value is " << i << endl;
       if (raw[i] != '|'){
-//	cout << "adding on a value " << raw[i] << endl;
-	//command = command + raw[i];
-	//command[count][p] = raw[i];
-	//command[count] += raw[i];
-	//curr[p] += raw[i];
-	
-//	cout << "current curr length is " << curr.length() << endl;
-	//cout << command[count] << endl;
-	//cout << "here????" << endl;
 	if(p == 0 && raw[i] == ' ' ){
 	  p--;
 	}
@@ -196,7 +133,6 @@ char ** parseLine(char * raw){
 	    valid =false;
 	  }
 	}
-//	cout << "after this part" << endl;
       }
       
       if(raw[i] == '|'){
@@ -211,24 +147,6 @@ char ** parseLine(char * raw){
 	    valid = false;
 	  }
 	
-// 	//string deliminator
-// 	string part = "";
-// 	int k = 0;
-// 	for (int j = 0; j < strlen(command); j++){
-// 	  if (command[j] != ' '){
-// 	    part += command[j];
-// 	  }
-// 	  else {
-// 	    tokens[index][argVal] = part;
-// 	    argVal += 1;
-// 	    part = "";
-// 	  }
-// 	  
-// 	}
-
-	//command[count] += '\0';
-	//command[count][p] = '\0';
-	//p = p +1;
 	curr += '\0';
 	
 	command[count] = (char *) malloc(curr.length() + 1 * sizeof(char));
@@ -238,14 +156,10 @@ char ** parseLine(char * raw){
 	}
 	p = 0;
 	count += 1;
-//	cout << "---------------------------------" << endl;
 	curr = "";
-	//tokens[index][argVal] = command;
+
 	cout << "here i am out of the for loop!" << endl;
-	//tokens[index] = command;
-	//index += 1;
 	
-	//command = "";
 	valid = true;
 	hasOut = 0;
 	hasIn = 0; 
@@ -253,13 +167,7 @@ char ** parseLine(char * raw){
 	previousOut = 0;
       }
     }
-  //  cout << "after the loop" << endl;
-//     if (pipecount == 0){
-//       //cout << command[count] << endl;
-//       cout << "why am i inside here" << pipecount << endl;
-//       //command[count][p] = '\0';
-//       //command[count] += '\0';
-//       cout << curr[0] << "first character" << curr.length() << endl;
+
       curr += '\0';
       cout << "curr value is " << curr << endl;
       command[count] = (char *) malloc(curr.length() + 1 * sizeof(char));
@@ -269,46 +177,12 @@ char ** parseLine(char * raw){
       
 	}
 	cout << command[count] << " THIS SHOULD BE THE SAME " << endl;
-      //command[count] = (char *) (strlen(curr) + 1 * sizeof(char));
-      //command[count] = curr;
-      
-    //  cout << command[0][0] << " first" << endl;
-     // cout << "it does equal 0!" << endl;
-     // cout << command[0] << " that was the command" << endl;
-      //cout << command[1] << "second command" << endl;
-      //tokens[index] = command;
-     // cout << "what am i doing?" << "index is " << index << tokens[index] << endl;
-    //}
-
-  //  cout << "here i am out of the for loop for real!" << endl;
-    //return tokens;
     return command;
   
 }
 
     
-  /*  
-    
-      if raw[i] !
-  currtoken = strtok(raw, "|");
-  while (currtoken != NULL){
-    tokens[index] = currtoken;
-    
-    index += 1;
-    
-    currtoken = strtok(NULL, "|");
   
-  }
-  
-  tokens[index] = NULL;
-  
-  
-  
-  tokens[index] = '\0';
-  return tokens;
-    
-  }
-}*/
 
 
 int main(int argF, char ** argv) {
@@ -321,17 +195,12 @@ int main(int argF, char ** argv) {
   
   
   char  buff[300];
-  //cwd = getcwd(buff,250);
   if (getcwd(buff,250) == NULL){
     //cout << "lolll" << endl;
   }
- // cout << buff << endl;
- // int t = 0;
-  while (1) {
+  commands:
+  do  {
     cout << "Start of loop" << endl;
-   // t = 1;
-  // time to read a line of input
-  //char * currentLine = inputParsing();
   char currentLine[110];
   string fullLine = "";
   if (argF > 1) {
@@ -341,12 +210,11 @@ int main(int argF, char ** argv) {
       fullLine += x;
     }
     
-      }/* Exit command detected - exit shell */
-//   else {
-//     cout << "> ";
-//     fullLine = getLine();
-//   }
-    /* Exit command detected - exit shell */
+      }
+   else {
+	getline(cin, fullLine);
+   }
+
     if (fullLine == "exit") {
       pipecount = 0;
       break;
@@ -356,15 +224,11 @@ int main(int argF, char ** argv) {
    if( argF > 1 && givenFile.eof()){
      exit(0);
    }
-/*  
- * 
-  if ((fgets(currentLine, sizeof(currentLine), stdin) == NULL) ){
-    break;
+  if (fullLine.length() > 100 ){
+    cout << "invalid input" << endl;
+    goto commands;
   }
   
-  if (strlen(currentLine) > 100){
-    cout << "invalid" << endl;
-  }*/
   fullLine = fullLine + " ";
   int y = 0;
   for (y = 0; y < fullLine.length(); y++){
@@ -375,36 +239,16 @@ int main(int argF, char ** argv) {
   cout << "before the current commands" << endl;
   
   char ** tokens = parseLine(currentLine);
-  //cout << pipecount << endl;
-  //cout << *tokens[0] << endl;
-  //cout << tokens[0] << endl;
   cout << "current line is " << currentLine <<endl;
- // cout << tokens[1] << endl;
-//   if (pipecount > 0) {
-//     pipearray = (int **) malloc(sizeof(int*) * (pipecount));
-//     cout << "here??" << endl;
-//     for (pipeid = 0; pipeid < pipecount; pipeid++) {
-//       pipearray[pipeid] = (int*)malloc(sizeof(int[2]));
-//       if (pipe(pipearray[pipeid]) != 0){
-// 	perror("pipe()");
-//       }
-//       }
-//   }
-  //cout << "the next hurtle" << endl;
-  //parse the line
-  //for each command in the line {
-  
+ 
   int p_id = 0;
- // int current = 0;
+ 
   int num_token_groups = pipecount + 1;
   int limit = pipecount + 1;
-  //int pipes2d[limit][2];
   int pipes[2];
-  //int pipes[num_token_groups][2];
 
    int pid_status[pipecount + 1];
    int in = 0;
-   //pipe(pipes);
   for (int current = 0; current < limit; current++){
 
 
@@ -420,18 +264,34 @@ int main(int argF, char ** argv) {
     int realSize = 0;
     
     cout << tokenized[0] << " " << tokenized[1] << " " << tokenized[2] << " " << tokenized[3];
-    //cout << tokenized[0] << "does the tokenizer work?" << endl;
-    //if (current == 0 ){
     pipe(pipes);
-   // }
-    //pipes2d[current][0] = pipes[0];
-    //pipes2d[current][1] = pipes[1];
+
 	cout << size << " is " << endl;
         cout << "current command index is " << current << endl;
     cout << "pipes 0 and pipes 1 " << pipes[0] << " " << pipes[1] << endl;
     
     for (int j = 0; j < size; j++){
+      
+	   bool contains_not_approved = (tokenized[j].find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234566789-./_") == string::npos);
+	   if(contains_not_approved == false){
+	     cout << "invalid input" << endl;
+	     goto commands;
+	   }
 	   cout <<  "WHY ARE WE NOT IN THIS LOOP" << endl;
+	   if (j == 0){
+	     string command = tokenized[j];
+	     if (command[0] != '/'){
+	       int l = 0;
+	       string cwd = "";
+	       while(l < 250 && buff[l] != '\0'){
+		    cwd = cwd + buff[l];
+	
+		    l = l + 1;   
+		    }
+		    command = cwd + "/" + command;
+		    tokenized[j] = command;
+		   }
+	   }
 	   if (tokenized[j] == "<") { 
 	      if (current != 0) {
 		cerr << "wrong pipe" << endl;
@@ -445,7 +305,7 @@ int main(int argF, char ** argv) {
 		  string cwd = "";
 		  while(l < 250 && buff[l] != '\0'){
 		    cwd = cwd + buff[l];
-		    //inputFile = buff[l] + inputFile[0];
+	
 		    l = l + 1;   
 		    }
 		    inputFile = cwd + "/" + inputFile;
@@ -471,7 +331,7 @@ int main(int argF, char ** argv) {
 	      string cwd = "";
  	      while(l < 250 && buff[l] != '\0'){
 		cwd = cwd + buff[l]; 
- 		//outputFile = buff[l] + outputFile;
+ 		
 	        l = l + 1;   
 	      }
 	      cout << "current output file is " << outputFile << endl;
@@ -487,29 +347,23 @@ int main(int argF, char ** argv) {
       if (realSize != 0){
 	size = realSize;
       }
-//     pipe(fd);
-//     pipes[current][0] = fd[0];
-//     pipes[current][1] = fd[1];
-//     
-    
-    ///  num_token_groups -= 1;
+
 
     pid = fork();
       cout << pid << " after da fork" << endl;
       if (pid == 0) {
-//	cout << "inside the child " << current << endl;
+
 	char ** args = (char**)malloc(sizeof(char*) *50);
 	for (int f = 0; f < size + 1; f++){
 	  args[f] = (char *) malloc(100 * sizeof(char));
 	}
-	//cout << "first arg, second arg is " << tokenized[0] << " " << tokenized[1] << endl;
+	
 	char * argv[size + 1];
 	int element = 0;
 	char * ready = "";
 	cout << "size for iterating is " << size << endl;
 	for( element = 0; element < size; element++){
-//	    cout << "element is " << element << endl;
-	   // argv[element] = tokenized[element].c_str();
+
 	    string ex = tokenized[element];
 	    if (ex == ">" || ex == "<" ){
 	      cout << "SHOULD NEVER SEE THIS" << endl;
@@ -517,16 +371,11 @@ int main(int argF, char ** argv) {
 	    }
 	    int g = 0;
 	    for (g = 0; g < ex.length(); g++){
-//	      cout << "string value is " << ex[g] << endl;
-	      //args[element] += (char) ex[g];
 	      args[element][g] = ex[g];
 	    }
 	   
 	    args[element][g] = '\0';
-	  //  cout << args[element];
 	    argv[element] = args[element];
-	   // cout << argv[0] << "current arg" <<  endl;
-	   // argv[element] = ready;
 	}
 	
 	argv[element] =  '\0';
@@ -538,82 +387,33 @@ int main(int argF, char ** argv) {
 		cerr << "time for some FILE INPUT" << endl;
 		int iF = open(inputFile.c_str(), O_RDONLY);
 		if (iF == -1) {
-		  cerr << strerror(errno) << endl;	
-		  
+		  cerr << strerror(errno) << endl;	  
 		  } 
-	//	 cout << "hereereer" << endl;
 		dup2(iF, STDIN_FILENO);
 		cout << "after the dup" << endl;
 	}
-	
-// 	  cout << "pipes[0] is " << pipes[0] << endl;
-// 	  //close(pipes[0]);
-// 	  int the_oF = pipes[1];
-// 	  cout << "the of " << the_oF << endl;
-// 	  dup2(pipes[1], STDOUT_FILENO);
-// 	  cout << "please " << endl;
-// 	  close(pipes[1]);
-// 	  cout << "here we GO" << endl;
 	  cout << "true love" << endl;
 	
 	}
       
 	if (pipecount > 0){
 	  	
-	//&& current < num_token_groups - 1
+	
 	if (current > 0  ){
 	  cout << "second command piper " << command << endl;
-// 	  int the_iF = pipes[current - 1][0];
-// 	  dup2(the_iF,STDIN_FILENO);
-// 	  close(pipes[current-1][1]);
-// 	
-	  //close(pipes[1]);
-	 //close(pipes[1]);
 	  dup2(in, STDIN_FILENO);
 
 	  close(pipes[0]);
 	  cerr << "LOL" << endl;
-// 	  
-// 	  //dup2(in, 0);
-// 	  //close(in);
-// 	  close(pipes2d[current-1][1]);
-// 	  dup2(pipes2d[current -1][0], STDIN_FILENO);
-// 	  close(pipes2d[current-1][0]);
 	}
 	
 	if ( current < num_token_groups - 1){
-	  //cout << "num - 1" << num_token_groups - 1 << endl;
-	 // cout << tokenized[current] << " before the pipe" << endl;
-	 // cout << "spitting out output into next pipe" << endl;
-// 	  int the_oF = pipes[current][1];
-// 	  cout << "output doesnt work" << endl;
-// 	  int error = dup2(the_oF,STDOUT_FILENO);
-// 	  cout << " error from dup is " << error;
-// 	  close(pipes[current][0]);
-// 	
-// 	  cout << "inside this one" << endl;
-// 	  
-// 	  close(pipes[0]);
-
-	  
 	  cerr << "set up write pipe" << endl;
-	 // close(pipes[0]);
 	  dup2(pipes[1], STDOUT_FILENO);
 	  close(pipes[1]);
-
-// 	  close(pipes2d[current][0]);
-// 	  cout << "lol" << endl;
-// 	  dup2(pipes2d[current][1], STDOUT_FILENO);
-// 	  cerr << "was it this?" << endl;
-// 	  close(pipes2d[current][1]);
 	    cerr << "made it here" << endl;
-	  
-	  
-	  //dup2(pipes[1], 1);
-	  //close(pipes[1]);
-	}
-	
 
+	}
 	
 	}
 	
@@ -626,23 +426,17 @@ int main(int argF, char ** argv) {
 	  }
 	  
 	  cerr << " tried to close " << endl;
-// 	  dup2(in, STDIN_FILENO);
-// 	  cout << "made it here" << endl;
-// 	  execvp(argv[0], argv);
 
 	}
       cerr << "------------------------------------------------------------------" << endl;
-    // if (current < num_token_groups - 1){
       int exVal = -1;
        cerr << "args are " << argv[0] << " " << argv[1] << " "  << argv[2] << " " << argv[3] << endl;
       cerr << "fsdfsf" << endl;
     
       int h = 0;
-      
-     // close(pipes[0]);
-     // close(pipes[1]);
-      execvp(argv[0], argv);
-      //exit(h);
+      h = execvp(argv[0], argv);
+      perror("exec failure");
+      exit(h);
       if (h == -1 ){
 	cerr << ":(" << endl;
       }
@@ -652,12 +446,8 @@ int main(int argF, char ** argv) {
       }
       }
     else{
-      //close(pipes[1]);
       cout << "pid value is " << p_id << " pid value is " << pid << endl;
       
-      //pipeid++;
-      //pid[p_id] = pid;
-
       close(pipes[1]);
       in = pipes[0];
       cout << "the parent" << endl;
@@ -666,31 +456,13 @@ int main(int argF, char ** argv) {
       cout << "finished eaiting" << endl;
       p_id++;
     }
-	
+  }
     for(int d = 0; d <= pipecount; d++){
       cout << "Process " << " exited with status " << pid_status[d] << endl;
     }
-  }
+  
 	  
-  }
+  } while(1);
   return 0;
 }
       
-    
-    
-//       do redirection stuff
-// 	execve ( command, args , ...);
-// 	oops, why did exec fail?
-//     } 
-//     else {
-//     store pid somewhere
-//     }
-//     }
-//   for each command in the line {
-//     waitpid (stored pid, &status);
-//     check return code placed in status;
-//   } 
-//   
-// 
-//   
-  
